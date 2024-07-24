dfb607185b05806cf164a2dc23bde6b436390716
night-pianist <sweetkhiet907@gmail.com>
2024-07-23 23:26:00 -0700

    update gradient + add some animation

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,4 +1,5 @@
 import { useNavigate, Link } from 'react-router-dom';
+import { useInView } from 'react-intersection-observer'; // scrolling effect
 import '../styling/landing.css';
 import logo from '../media/logo.png'
 import bg from '../media/bg.png'
@@ -10,6 +11,11 @@ const LandingPage = () => {
     const aboutOnClick = () => { navigate('/about'); } // go to the about page
     const creationsOnClick = () => { navigate('/creations'); } // go to the creations page
 
+    const { ref: ref1, inView: inView1 } = useInView({ triggerOnce: true, threshold: 0.1 });
+    const { ref: ref2, inView: inView2 } = useInView({ triggerOnce: true, threshold: 0.1 });
+    const { ref: ref3, inView: inView3 } = useInView({ triggerOnce: true, threshold: 0.1 });
+    const { ref: ref4, inView: inView4 } = useInView({ triggerOnce: true, threshold: 0.1 });
+
     return ( 
         <div className="landing-page">
             <img 
@@ -17,7 +23,7 @@ const LandingPage = () => {
                 alt="bg" 
                 className="bg-pic" 
             />
-            <div className="navbar absolute w-[99%]">
+            <div className="navbar absolute w-[99%] gradient-shadow">
                 <div className="relative flex items-center">
                     <img 
                         src={logo} 
@@ -27,14 +33,14 @@ const LandingPage = () => {
                     />
                     <div className="btns-container absolute top-0 right-0 flex justify-end space-x-4" style={{ padding: '1%'}}>
                         <button // about button 
-                            className="about-btn bg-ice-blue sm:text-sm md:text-md lg:text-lg rounded-md"
+                            className="about-btn text-outline tracking-wide text-white sm:text-sm md:text-md lg:text-lg rounded-md"
                             style={{ padding: '5%' }}
                             onClick={aboutOnClick}
                         > 
                             creations
                         </button>
                         <button // creations button
-                            className="creation-btn bg-ice-blue sm:text-sm md:text-md lg:text-lg rounded-md"
+                            className="creation-btn text-outline tracking-wide text-white sm:text-sm md:text-md lg:text-lg rounded-md"
                             style={{ padding: '5%' }}
                             onClick={creationsOnClick}
                         >
@@ -45,20 +51,45 @@ const LandingPage = () => {
             </div>
             <div className="welcome">
                 <h1 
-                    className="sm:text-3xl md:text-4xl lg:text-5xl text-white" 
+                    className="sm:text-3xl md:text-4xl lg:text-5xl text-white pl-10 pt-40" 
                     style={{ marginLeft: '3%' }} 
                 >
                     welcome
                 </h1>
                 <h1 
-                    className="welcome sm:text-3xl md:text-4xl lg:text-5xl text-white" 
+                    className="sm:text-3xl md:text-4xl lg:text-5xl text-white pl-10" 
                     style={{ marginLeft: '3%' }} 
                 >
                     to my canvas!
                 </h1>
+                {/* <h1 
+                    className="welcome sm:text-3xl md:text-4xl lg:text-5xl text-white" 
+                    style={{ marginLeft: '3%' }} 
+                >
+                    to my canvas!
+                </h1> */}
             </div>
-            <div className="main-content text-white">
-                i'm khiet!
+            <div className="transition">
+
+            </div>
+            <div className="main-content text-white sm:text-5xl md:text-6xl lg:text-7xl flex flex-vertical justify-center" style={{ padding: '3%'}}>
+                <div className="title">
+                    i'm khiet!
+                </div>
+                <div className="overflow-hidden">
+                    <div ref={ref1} className={`animated-div ${inView1 ? 'animate-left' : ''}`}>
+                        <p>I’m an artist...</p>
+                    </div>
+                    <div ref={ref2} className={`animated-div ${inView2 ? 'animate-right' : ''}`}>
+                        <p>I’m a CAD technician...</p>
+                    </div>
+                    <div ref={ref3} className={`animated-div ${inView3 ? 'animate-left' : ''}`}>
+                        <p>I’m a programmer...</p>
+                    </div>
+                    <div ref={ref4} className={`animated-div ${inView4 ? 'animate-right' : ''}`}>
+                        <p>I’m a music lover...</p>
+                    </div>
+                </div>
             </div>
         </div>
      );
