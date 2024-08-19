45b10dfeb92594fe72b0584eab0154d2ed4f5e71
night-pianist <sweetkhiet907@gmail.com>
2024-08-18 21:31:16 -0700

    set up cad page

--- src/components/CAD.tsx
+++ src/components/CAD.tsx
@@ -4,36 +4,23 @@ import AnimatedText from './AnimatedWelcome';
 import '../styling/styles.css';
 
 // images
-import img1 from '../media/art/northernlights.png';
-import img2 from '../media/art/acotar.png';
-import img3 from '../media/art/waterfall.png';
-import img4 from '../media/art/ocean.png';
-import img5 from '../media/art/dreams.png';
-import img6 from '../media/art/tcp.png';
-import img7 from '../media/art/tear.png';
-import img8 from '../media/art/pastel.png';
-import img9 from '../media/art/bridge.png';
+import img1 from '../media/cad/ewh.png';
+import img2 from '../media/cad/booknook.png';
+import img3 from '../media/cad/batarang.png';
 
 interface Image {
     src: string;
     alt: string;
     title: string;
-    medium: string;
     date: string;
     description: string;
     path: string;
   }  
 
 const images: Image[] = [
-    { src: img1, alt: 'Image 1', title: 'northern lights', medium: 'prisma colors', date: 'july 2021', description: '', path: 'northern-lights' },
-    { src: img2, alt: 'Image 2', title: 'fantasy', medium: 'digital drawing', date: 'august 2021', description: '', path: 'fantasy' },
-    { src: img3, alt: 'Image 3', title: 'bright days', medium: 'prisma colors', date: 'july 2023', description: '', path: 'bright-days' },
-    { src: img4, alt: 'Image 4', title: 'ocean', medium: 'prisma colors', date: 'october 2021', description: '', path: 'ocean' },
-    { src: img5, alt: 'Image 5', title: 'dreams', medium: 'prisma colors', date: 'october 2021', description: '', path: 'dreams' },
-    { src: img6, alt: 'Image 6', title: 'faerie forest', medium: 'prisma colors', date: 'june 2022', description: 'des', path: 'faerie-forest' },
-    { src: img7, alt: 'Image 7', title: 'reflection', medium: 'prisma colors', date: 'november 2021', description: 'des', path: 'reflection' },
-    { src: img8, alt: 'Image 8', title: 'pastel days', medium: 'prisma colors', date: 'september 2023', description: 'des', path: 'pastel-days' },
-    { src: img9, alt: 'Image 9', title: 'bridge', medium: 'prisma colors', date: 'march 2024', description: 'des', path: 'bridge' },
+    { src: img1, alt: 'Image 1', title: 'oxygen tank encasing', date: 'may 2022', description: '', path: 'encasing' },
+    { src: img2, alt: 'Image 2', title: 'book nook', date: 'june 2023', description: '', path: 'book-nook' },
+    { src: img3, alt: 'Image 3', title: 'batarang', date: 'january 2024', description: '', path: 'batarang' },
   ]; 
 
 const CAD = () => {
@@ -108,27 +95,90 @@ const CAD = () => {
                     </h1>
                 </div>
                 <div className={`${fadeIn ? 'fade-in' : ''}`}>
-                    <div className="grid grid-cols-3 gap-4"> 
-                    {images.map((image, index) => ( // collage of drawings
+                  <div className="grid grid-cols-10 gap-4">
+                    {/* First image: horizontal, spans across the first 6 columns */}
+                    <div className="col-span-6">
+                      <div
+                        className="relative overflow-hidden rounded-lg shadow-lg group cursor-pointer"
+                        onClick={() => handleGalleryImageClick(0)}
+                      >
+                        <img
+                          src={images[0].src}
+                          alt={images[0].alt}
+                          className="w-full h-full object-cover transform group-hover:scale-100 transition-transform duration-300"
+                        />
+                        <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-end justify-center">
+                          <p className="text-white sm:text-sm md:text-md lg:text-lg p-4 font-varelaround">{images[0].title}</p>
+                        </div>
+                      </div>
+                    </div>
+
+                    {/* Second image: vertical, spans across 3 columns and takes up 2 rows */}
+                    <div className="col-span-4 row-span-2">
+                      <div
+                        className="relative overflow-hidden rounded-lg shadow-lg group cursor-pointer h-full"
+                        onClick={() => handleGalleryImageClick(1)}
+                      >
+                        <img
+                          src={images[1].src}
+                          alt={images[1].alt}
+                          className="w-full h-full object-cover transform group-hover:scale-100 transition-transform duration-300"
+                        />
+                        <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-end justify-center">
+                          <p className="text-white sm:text-sm md:text-md lg:text-lg p-4 font-varelaround">{images[1].title}</p>
+                        </div>
+                      </div>
+                    </div>
+
+                    {/* Third image: horizontal, spans across the first 7 columns on the next row */}
+                    <div className="col-span-6">
+                      <div
+                        className="relative overflow-hidden rounded-lg shadow-lg group cursor-pointer"
+                        onClick={() => handleGalleryImageClick(2)}
+                      >
+                        <img
+                          src={images[2].src}
+                          alt={images[2].alt}
+                          className="w-full h-full object-cover transform group-hover:scale-100 transition-transform duration-300"
+                        />
+                        <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-end justify-center">
+                          <p className="text-white sm:text-sm md:text-md lg:text-lg p-4 font-varelaround">{images[2].title}</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className={`m-[7%] ${fadeIn ? 'fade-in' : ''}`}>
+                        <h1 className="font-dancingscript text-white sm:text-5xl md:text-6xl lg:text-7xl">
+                            more about each design...
+                        </h1>
+                    </div>
+
+                    <div className={`container mx-auto text-center ${fadeIn ? 'fade-in' : ''}`}> 
+                        {images.map((image, index) => (
                         <div
                             key={index}
-                            className="relative overflow-hidden rounded-lg shadow-lg group cursor-pointer"
-                            onClick={() => handleGalleryImageClick(index)}
+                            id={image.path}
+                            ref={(el) => (descriptionRefs.current[index] = el)}
+                            className="my-8 flex flex-col items-center font-varelaround text-white"
                         >
                             <img
                                 src={image.src}
                                 alt={image.alt}
-                                className="w-full h-full object-cover transform group-hover:scale-100 transition-transform duration-300"
+                                className="w-full max-w-4xl h-auto rounded-lg shadow-lg mb-4"
                             />
-                            <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-end justify-center">
-                                <p className="text-white sm:text-sm md:text-md lg:text-lg p-4 font-varelaround">{image.title}</p>
-                            </div>
+                            <h2 className="sm:text-2xl md:text-3xl lg:text-4xl font-bold italic p-[1%]">{image.title}</h2>
+                            {/* <h3 className="sm:text-md md:text-xl lg:text-2xl">medium: {image.medium}</h3> */}
+                            <h3 className="sm:text-md md:text-xl lg:text-2xl pb-[5%]">completed: {image.date}</h3>
                         </div>
-                    ))}
+                        ))}
                     </div>
-                    
+
                 </div>
+
             </div>
+
+
 
         </div> 
     );
