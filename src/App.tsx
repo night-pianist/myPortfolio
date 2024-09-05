dd8efd5e9407795032804252909c098500adab8f
night-pianist <sweetkhiet907@gmail.com>
2024-09-04 22:38:48 -0700

    add page refresh

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import About from './components/About';
 import Artwork from './components/Artwork';
 import Programs from './components/Programs';
 import CAD from './components/CAD';
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Router> 
+          <ScrollToTop /> 
           <Routes>
             <Route path="/" element={<LandingPage />}/>
             <Route path="/about" element={<About />}/>
