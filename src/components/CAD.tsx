197347c66f81c4bb1a077d5a8ab02e9e272305f3
night-pianist <sweetkhiet907@gmail.com>
2024-09-27 15:42:43 -0700

    update cad page

--- src/components/CAD.tsx
+++ src/components/CAD.tsx
@@ -7,20 +7,34 @@ import '../styling/styles.css';
 import img1 from '../media/cad/ewh.png';
 import img2 from '../media/cad/booknook.png';
 import img3 from '../media/cad/batarang.png';
+import img4 from '../media/cad/piano.png';
+import img5 from '../media/cad/satellite.png';
+import img6 from '../media/cad/snowman.png';
+import img7 from '../media/cad/gingerbread.png';
+import img8 from '../media/cad/padHolder.png';
+import img9 from '../media/cad/octopus.png';
 
 interface Image {
     src: string;
     alt: string;
     title: string;
     date: string;
+    software: string;
     description: string;
     path: string;
   }  
 
 const images: Image[] = [
-    { src: img1, alt: 'Image 1', title: 'oxygen tank encasing', date: 'may 2022', description: 'I designed this for an engineering competition I won :)', path: 'encasing' },
-    { src: img2, alt: 'Image 2', title: 'book nook', date: 'january 2024', description: 'I love reading books, and I wanted to design a book nook I could use for my bookshelf that illustrated a setting from a book', path: 'book-nook' },
-    { src: img3, alt: 'Image 3', title: 'batarang', date: 'june 2023', description: 'I designed this for a friend', path: 'batarang' },
+    { src: img1, alt: 'Image 1', title: 'oxygen tank encasing', date: 'may 2022', software: 'fusion360', description: 'I designed this for an engineering competition I won :)', path: 'encasing' },
+    { src: img2, alt: 'Image 2', title: 'book nook', date: 'january 2024', software: 'fusion360', description: 'I love reading books, and I wanted to design a book nook I could use for my bookshelf that illustrated a setting from a book', path: 'book-nook' },
+    { src: img3, alt: 'Image 3', title: 'batarang', date: 'june 2023',software: 'fusion360', description: 'I designed this for a friend', path: 'batarang' },
+    { src: img4, alt: 'Image 1', title: 'piano', date: 'july 2023', software: 'fusion360', description: 'I designed this because I love playing piano', path: 'encasing' },
+    { src: img5, alt: 'Image 2', title: 'satellite', date: 'august 2023', software: 'fusion360', description: 'I was inspired by an origami satellite I saw', path: 'book-nook' },
+    { src: img6, alt: 'Image 3', title: 'snowman', date: 'november 2023',software: 'fusion360', description: 'During the holiday season, I wanted to design decor', path: 'batarang' },
+    { src: img7, alt: 'Image 1', title: 'gingerbread', date: 'march 2024', software: 'fusion360', description: 'I wanted to learn more about joints, so I designed this based on an image of a CAD I found online', path: 'encasing' },
+    { src: img8, alt: 'Image 2', title: 'holder', date: 'november 2023', software: 'fusion360', description: 'I wanted to design something practical', path: 'book-nook' },
+    { src: img9, alt: 'Image 3', title: 'octopus', date: 'april 2024',software: 'fusion360', description: 'I designed this for my dorm decor', path: 'batarang' },
+    
   ]; 
 
 const CAD = () => {
@@ -113,7 +127,7 @@ const CAD = () => {
                       </div>
                     </div>
 
-                    {/* Second image: vertical, spans across 3 columns and takes up 2 rows */}
+                    {/* Second image: vertical, spans across 4 columns and 2 rows */}
                     <div className="col-span-4 row-span-2">
                       <div
                         className="relative overflow-hidden rounded-lg shadow-lg group cursor-pointer h-full"
@@ -130,7 +144,7 @@ const CAD = () => {
                       </div>
                     </div>
 
-                    {/* Third image: horizontal, spans across the first 7 columns on the next row */}
+                    {/* Third image: horizontal, spans across the first 6 columns */}
                     <div className="col-span-6">
                       <div
                         className="relative overflow-hidden rounded-lg shadow-lg group cursor-pointer"
@@ -147,6 +161,108 @@ const CAD = () => {
                       </div>
                     </div>
                   </div>
+
+                  <div className="grid grid-cols-3 gap-4 pt-4">
+                    <div className="col-span-1">
+                      <div
+                        className="relative overflow-hidden rounded-lg shadow-lg group cursor-pointer h-64"
+                        onClick={() => handleGalleryImageClick(3)}
+                      >
+                        <img
+                          src={images[3].src}
+                          alt={images[3].alt}
+                          className="w-full h-full object-cover transform group-hover:scale-100 transition-transform duration-300"
+                        />
+                        <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-end justify-center">
+                          <p className="text-white sm:text-sm md:text-md lg:text-lg p-4 font-varelaround">{images[3].title}</p>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="col-span-1">
+                      <div
+                        className="relative overflow-hidden rounded-lg shadow-lg group cursor-pointer h-64"
+                        onClick={() => handleGalleryImageClick(4)}
+                      >
+                        <img
+                          src={images[4].src}
+                          alt={images[4].alt}
+                          className="w-full h-full object-cover transform group-hover:scale-100 transition-transform duration-300"
+                        />
+                        <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-end justify-center">
+                          <p className="text-white sm:text-sm md:text-md lg:text-lg p-4 font-varelaround">{images[4].title}</p>
+                        </div>
+                      </div>
+                    </div>
+
+                    {/* Fourth row: 3 more images with equal spacing */}
+                    <div className="col-span-1">
+                      <div
+                        className="relative overflow-hidden rounded-lg shadow-lg group cursor-pointer h-64"
+                        onClick={() => handleGalleryImageClick(5)}
+                      >
+                        <img
+                          src={images[5].src}
+                          alt={images[5].alt}
+                          className="w-full h-full object-cover transform group-hover:scale-100 transition-transform duration-300"
+                        />
+                        <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-end justify-center">
+                          <p className="text-white sm:text-sm md:text-md lg:text-lg p-4 font-varelaround">{images[5].title}</p>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="col-span-1">
+                      <div
+                        className="relative overflow-hidden rounded-lg shadow-lg group cursor-pointer h-64"
+                        onClick={() => handleGalleryImageClick(6)}
+                      >
+                        <img
+                          src={images[6].src}
+                          alt={images[6].alt}
+                          className="w-full h-full object-cover transform group-hover:scale-100 transition-transform duration-300"
+                        />
+                        <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-end justify-center">
+                          <p className="text-white sm:text-sm md:text-md lg:text-lg p-4 font-varelaround">{images[6].title}</p>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="col-span-1">
+                      <div
+                        className="relative overflow-hidden rounded-lg shadow-lg group cursor-pointer h-64"
+                        onClick={() => handleGalleryImageClick(7)}
+                      >
+                        <img
+                          src={images[7].src}
+                          alt={images[7].alt}
+                          className="w-full h-full object-cover transform group-hover:scale-100 transition-transform duration-300"
+                        />
+                        <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-end justify-center">
+                          <p className="text-white sm:text-sm md:text-md lg:text-lg p-4 font-varelaround">{images[7].title}</p>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="col-span-1">
+                      <div
+                        className="relative overflow-hidden rounded-lg shadow-lg group cursor-pointer h-64"
+                        onClick={() => handleGalleryImageClick(8)}
+                      >
+                        <img
+                          src={images[8].src}
+                          alt={images[8].alt}
+                          className="w-full h-full object-cover transform group-hover:scale-100 transition-transform duration-300"
+                        />
+                        <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-end justify-center">
+                          <p className="text-white sm:text-sm md:text-md lg:text-lg p-4 font-varelaround">{images[8].title}</p>
+                        </div>
+                      </div>
+                    </div>
+                    
+                  </div>
+
+
 
                   <div className={`m-[7%] ${fadeIn ? 'fade-in' : ''}`}>
                         <h1 className="font-dancingscript text-white sm:text-5xl md:text-6xl lg:text-7xl">
